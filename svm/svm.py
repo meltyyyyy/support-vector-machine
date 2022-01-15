44e9740af411f04a3ccfdc6cbfffcd1cc7ce5881
takeru.abe <takeru.abe@gaiax.com>
2022-01-15 10:11:37 +0900

    implement linear-model onto 3d datasets

--- svm/svm.py
+++ svm/svm.py
@@ -11,11 +11,18 @@ def execute():
     y = y % 2
     X_new = np.hstack([X, X[:, 1:] ** 2])
 
-    linear_svm = LinearSVC().fit(X, y)
+    linear_svm_3d = LinearSVC().fit(X_new, y)
+    coef, intercept = linear_svm_3d.coef_.ravel(), linear_svm_3d.intercept_
 
     fig = plt.figure()
     ax = Axes3D(fig, elev=-152, azim=-26)
+    xx = np.linspace(X_new[:, 0].min() - 2, X_new[:, 0].max() + 2, 50)
+    yy = np.linspace(X_new[:, 1].min() - 2, X_new[:, 1].max() + 2, 50)
+
     mask = y == 0
+    XX, YY = np.meshgrid(xx, yy)
+    ZZ = (coef[0] * XX + coef[1] * YY + intercept) / -coef[2]
+    ax.plot_surface(XX, YY, ZZ, rstride=8, cstride=8, alpha=0.3)
     ax.scatter(X_new[mask, 0], X_new[mask, 1], X_new[mask, 2], c='b', cmap=mglearn.cm2, s=60)
     ax.scatter(X_new[~mask, 0], X_new[~mask, 1], X_new[~mask, 2], c='r', marker='^', cmap=mglearn.cm2, s=60)
     ax.set_xlabel("Feature 0")
