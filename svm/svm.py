e66f0c08cae0aa0c5eb649c755cf4bfa12c1ff31
takeru.abe <takeru.abe@gaiax.com>
2022-01-15 10:00:56 +0900

    create extended dataset

--- svm/svm.py
+++ svm/svm.py
@@ -1,16 +1,24 @@
 import mglearn.plots
+import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.datasets import make_blobs
 from sklearn.svm import LinearSVC
+
 
 def execute():
     X, y = make_blobs(centers=4, random_state=8)
     y = y % 2
+    X_new = np.hstack([X, X[:, 1:] ** 2])
 
     linear_svm = LinearSVC().fit(X, y)
 
-    mglearn.plots.plot_2d_separator(linear_svm, X)
-    mglearn.discrete_scatter(X[:, 0], X[:, 1], y)
-    plt.xlabel("Feature 0")
-    plt.ylabel("Feature 1")
-    plt.show()
+    fig = plt.figure()
+    ax = Axes3D(fig, elev=-152, azim=-26)
+    mask = y == 0
+    ax.scatter(X_new[mask, 0], X_new[mask, 1], X_new[mask, 2], c='b', cmap=mglearn.cm2, s=60)
+    ax.scatter(X_new[~mask, 0], X_new[~mask, 1], X_new[~mask, 2], c='r', marker='^', cmap=mglearn.cm2, s=60)
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+    ax.set_zlabel("Feature1 ** 2")
+    fig.savefig('svm/svm.png')
